E3SM-Project/E3SM

Rework snowcapping (from CESMv2)
If use_extrasnowlayers, then a new snowcapping routine will be
used, which is contained in SnowHydrologyMod.  The old (current)
snowcapping scheme touches multiple files, and causes a column
in a snow capped state to have a virtual roof over the snow.
Excess snow is sent either to a runoff term or an ice sheet source
term (positive surface mass balance), but new snow does not
accumulate at the surface.  This causes the snow to age as
if no new snowfall is occuring and leads to biases in its density.

The new routine, adapted from CLMv5, removes excess snow from
the bottom of the snow column, allowing the snowpack to refresh
from the top like it would in nature.  This commit places control
logic surrounding the relevant snow capping infrastructure so that
if use_extrasnowlayers is true, then the new routine will be toggled
while maintaining conservation of mass.

components/clm/src/biogeophys/BalanceCheckMod.F90
@@ -9,7 +9,7 @@ module BalanceCheckMod
   use shr_log_mod        , only : errMsg => shr_log_errMsg
   use decompMod          , only : bounds_type
   use abortutils         , only : endrun
-  use clm_varctl         , only : iulog, use_var_soil_thick
+  use clm_varctl         , only : iulog, use_var_soil_thick, use_extrasnowlayers
   use clm_varcon         , only : namep, namec
   use GetGlobalValuesMod , only : GetGlobalIndex
   use atm2lndType        , only : atm2lnd_type
@@ -444,37 +444,53 @@ contains
                      + qflx_snwcp_ice(c) + qflx_snwcp_liq(c) + qflx_sl_top_soil(c)
 
                 if (lun_pp%itype(l) == istdlak) then 
-                   if ( do_capsnow(c) ) then
+                   if ( do_capsnow(c) .and. .not. use_extrasnowlayers) then
                       snow_sources(c) = qflx_snow_grnd_col(c) &
                            + frac_sno_eff(c) * (qflx_dew_snow(c) + qflx_dew_grnd(c) ) 
 
                       snow_sinks(c)   = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c) ) &
                            + (qflx_snwcp_ice(c) + qflx_snwcp_liq(c) - qflx_prec_grnd(c))  &
                            + qflx_snow_melt(c)  + qflx_sl_top_soil(c)
-                   else
+                   else if (.not. use_extrasnowlayers) then
                       snow_sources(c) = qflx_snow_grnd_col(c) &
                            + frac_sno_eff(c) * (qflx_rain_grnd_col(c) &
                            +  qflx_dew_snow(c) + qflx_dew_grnd(c) ) 
 
                       snow_sinks(c)  = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c) ) &
                            + qflx_snow_melt(c)  + qflx_sl_top_soil(c)
+                   else if (use_extrasnowlayers) then !++ams i.e., firn model
+                      snow_sources(c) = qflx_snow_grnd_col(c) &
+                           + frac_sno_eff(c) * (qflx_rain_grnd_col(c) &
+                           +  qflx_dew_snow(c) + qflx_dew_grnd(c) ) 
+
+                      snow_sinks(c)  = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c) ) &
+                           + qflx_snwcp_ice(c) + qflx_snwcp_liq(c) &
+                           + qflx_snow_melt(c) + qflx_sl_top_soil(c)
                    endif
                 endif
 
                 if (lun_pp%itype(l) == istsoil .or. lun_pp%itype(l) == istcrop .or. lun_pp%itype(l) == istwet ) then
-                   if ( do_capsnow(c) ) then
+                   if ( do_capsnow(c) .and. .not. use_extrasnowlayers) then
                       snow_sources(c) = frac_sno_eff(c) * (qflx_dew_snow(c) + qflx_dew_grnd(c) ) &
                            + qflx_h2osfc_to_ice(c) + qflx_prec_grnd(c)
 
                       snow_sinks(c) = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c)) &
                            + qflx_snwcp_ice(c) + qflx_snwcp_liq(c) &
                            + qflx_snow_melt(c) + qflx_sl_top_soil(c)
-                   else
+                   else if (.not. use_extrasnowlayers) then
                       snow_sources(c) = (qflx_snow_grnd_col(c) - qflx_snow_h2osfc(c) ) &
                            + frac_sno_eff(c) * (qflx_rain_grnd_col(c) &
                            +  qflx_dew_snow(c) + qflx_dew_grnd(c) ) + qflx_h2osfc_to_ice(c)
 
                       snow_sinks(c) = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c)) &
+                           + qflx_snow_melt(c) + qflx_sl_top_soil(c)
+                   else if (use_extrasnowlayers) then !++ams i.e., firn model
+                      snow_sources(c) = (qflx_snow_grnd_col(c) - qflx_snow_h2osfc(c) ) &
+                           + frac_sno_eff(c) * (qflx_rain_grnd_col(c) &
+                           +  qflx_dew_snow(c) + qflx_dew_grnd(c) ) + qflx_h2osfc_to_ice(c)
+
+                      snow_sinks(c) = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c)) &
+                           + qflx_snwcp_ice(c) + qflx_snwcp_liq(c) &
                            + qflx_snow_melt(c) + qflx_sl_top_soil(c)
                    endif
                 endif

components/clm/src/biogeophys/HydrologyNoDrainageMod.F90
@@ -7,7 +7,7 @@ Module HydrologyNoDrainageMod
   use shr_kind_mod      , only : r8 => shr_kind_r8
   use shr_log_mod       , only : errMsg => shr_log_errMsg
   use decompMod         , only : bounds_type
-  use clm_varctl        , only : iulog, use_vichydro
+  use clm_varctl        , only : iulog, use_vichydro, use_extrasnowlayers
   use clm_varcon        , only : e_ice, denh2o, denice, rpi, spval
   use atm2lndType       , only : atm2lnd_type
   use AerosolType       , only : aerosol_type
@@ -70,7 +70,7 @@ contains
     use clm_varctl           , only : use_cn, use_betr, use_fates, use_pflotran, pf_hmode
     use clm_varpar           , only : nlevgrnd, nlevsno, nlevsoi, nlevurb
     use clm_time_manager     , only : get_step_size, get_nstep
-    use SnowHydrologyMod     , only : SnowCompaction, CombineSnowLayers, DivideSnowLayers
+    use SnowHydrologyMod     , only : SnowCompaction, CombineSnowLayers, DivideSnowLayers, SnowCapping
     use SnowHydrologyMod     , only : SnowWater, BuildSnowFilter 
     use SoilHydrologyMod     , only : CLMVICMap, SurfaceRunoff, Infiltration, WaterTable
     use SoilWaterMovementMod , only : SoilWater 
@@ -297,6 +297,12 @@ contains
          !Jinyun Tang, Feb 4, 2015
          call ep_betr%CalcDewSubFlux(bounds, col_pp, num_hydrologyc, filter_hydrologyc)
       endif           
+      
+      if (use_extrasnowlayers) then
+         call SnowCapping(bounds, num_nolakec, filter_nolakec, num_snowc, filter_snowc, &
+                          aerosol_vars, waterflux_vars, waterstate_vars)
+      end if
+      
       ! Natural compaction and metamorphosis.
       call SnowCompaction(bounds, num_snowc, filter_snowc, &
            temperature_vars, waterstate_vars, top_as)

components/clm/src/biogeophys/LakeFluxesMod.F90
@@ -50,7 +50,7 @@ contains
     use clm_varpar          , only : nlevlak
     use clm_varcon          , only : hvap, hsub, hfus, cpair, cpliq, tkwat, tkice, tkair
     use clm_varcon          , only : sb, vkc, grav, denh2o, tfrz, spval, zsno
-    use clm_varctl          , only : use_lch4
+    use clm_varctl          , only : use_lch4, use_extrasnowlayers
     use LakeCon             , only : betavis, z0frzlake, tdmax, emg_lake
     use LakeCon             , only : lake_use_old_fcrit_minz0
     use LakeCon             , only : minz0lake, cur0, cus, curm, fcrit
@@ -634,10 +634,12 @@ contains
          qflx_dirct_rain(p) = 0._r8
          qflx_leafdrip(p) = 0._r8
 
-         ! Because they will be used in pft2col initialize here.
-         ! This will be overwritten in LakeHydrology
-         qflx_snwcp_ice(p) = 0._r8
-         qflx_snwcp_liq(p) = 0._r8
+         if (.not. use_extrasnowlayers) then
+            ! Because they will be used in pft2col initialize here.
+            ! This will be overwritten in LakeHydrology
+            qflx_snwcp_ice(p) = 0._r8
+            qflx_snwcp_liq(p) = 0._r8
+         end if
 
       end do
 

components/clm/src/biogeophys/LakeHydrologyMod.F90
@@ -73,10 +73,10 @@ contains
     ! !USES:
     use clm_varcon      , only : denh2o, denice, spval, hfus, tfrz, cpliq, cpice
     use clm_varpar      , only : nlevsno, nlevgrnd, nlevsoi
-    use clm_varctl      , only : iulog
+    use clm_varctl      , only : iulog, use_extrasnowlayers
     use clm_time_manager, only : get_step_size
     use SnowHydrologyMod, only : SnowCompaction, CombineSnowLayers, SnowWater, BuildSnowFilter
-    use SnowHydrologyMod, only : DivideSnowLayers
+    use SnowHydrologyMod, only : DivideSnowLayers, SnowCapping
     use LakeCon         , only : lsadz
     !
     ! !ARGUMENTS:
@@ -247,15 +247,19 @@ contains
 
          qflx_dirct_rain(p) = 0._r8
          qflx_leafdrip(p) = 0._r8
-
-         if (do_capsnow(c)) then
-            qflx_snwcp_ice(p) = qflx_prec_grnd_snow(p)
-            qflx_snwcp_liq(p) = qflx_prec_grnd_rain(p)
-            qflx_snow_grnd_patch(p) = 0._r8
-            qflx_rain_grnd(p) = 0._r8
+         if (.not. use_extrasnowlayers) then
+            if (do_capsnow(c)) then
+               qflx_snwcp_ice(p) = qflx_prec_grnd_snow(p)
+               qflx_snwcp_liq(p) = qflx_prec_grnd_rain(p)
+               qflx_snow_grnd_patch(p) = 0._r8
+               qflx_rain_grnd(p) = 0._r8
+            else
+               qflx_snwcp_ice(p) = 0._r8
+               qflx_snwcp_liq(p) = 0._r8
+               qflx_snow_grnd_patch(p) = qflx_prec_grnd_snow(p)           ! ice onto ground (mm/s)
+               qflx_rain_grnd(p)     = qflx_prec_grnd_rain(p)           ! liquid water onto ground (mm/s)
+            end if
          else
-            qflx_snwcp_ice(p) = 0._r8
-            qflx_snwcp_liq(p) = 0._r8
             qflx_snow_grnd_patch(p) = qflx_prec_grnd_snow(p)           ! ice onto ground (mm/s)
             qflx_rain_grnd(p)     = qflx_prec_grnd_rain(p)           ! liquid water onto ground (mm/s)
          end if
@@ -275,7 +279,7 @@ contains
          ! U.S.Department of Agriculture Forest Service, Project F,
          ! Progress Rep. 1, Alta Avalanche Study Center:Snow Layer Densification.
 
-         if (do_capsnow(c)) then
+         if (do_capsnow(c) .and. .not. use_extrasnowlayers) then
             dz_snowf = 0._r8
          else
             if (forc_t(t) > tfrz + 2._r8) then
@@ -368,7 +372,7 @@ contains
             ! Update the pft-level qflx_snowcap
             ! This was moved in from Hydrology2 to keep all pft-level
             ! calculations out of Hydrology2
-            if (do_capsnow(c)) then
+            if (do_capsnow(c) .and. .not. use_extrasnowlayers) then
                qflx_snwcp_ice(p) = qflx_snwcp_ice(p) + qflx_dew_snow(p) 
                qflx_snwcp_liq(p) = qflx_snwcp_liq(p) + qflx_dew_grnd(p)
             end if
@@ -390,7 +394,7 @@ contains
             ! Update snow pack for dew & sub.
 
             h2osno_temp = h2osno(c)
-            if (do_capsnow(c)) then
+            if (do_capsnow(c) .and. .not. use_extrasnowlayers) then
                h2osno(c) = h2osno(c) - qflx_sub_snow(p)*dtime
                qflx_snwcp_ice(p) = qflx_snwcp_ice(p) + qflx_dew_snow(p) 
                qflx_snwcp_liq(p) = qflx_snwcp_liq(p) + qflx_dew_grnd(p)
@@ -406,9 +410,10 @@ contains
             h2osno(c) = max(h2osno(c), 0._r8)
          end if
 
-         qflx_snwcp_ice_col(c) = qflx_snwcp_ice(p)
-         qflx_snwcp_liq_col(c) = qflx_snwcp_liq(p)
-
+         if (.not. use_extrasnowlayers) then
+            qflx_snwcp_ice_col(c) = qflx_snwcp_ice(p)
+            qflx_snwcp_liq_col(c) = qflx_snwcp_liq(p)
+         end if
 
       end do
 
@@ -452,6 +457,11 @@ contains
       call SnowWater(bounds, &
            num_shlakesnowc, filter_shlakesnowc, num_shlakenosnowc, filter_shlakenosnowc, &
            atm2lnd_vars, waterflux_vars, waterstate_vars, aerosol_vars)
+           
+      if (use_extrasnowlayers) then
+         call SnowCapping(bounds, num_lakec, filter_lakec, num_shlakesnowc, filter_shlakesnowc, &
+                          aerosol_vars, waterflux_vars, waterstate_vars)
+      end if
 
       ! Determine soil hydrology
       ! Here this consists only of making sure that soil is saturated even as it melts and
@@ -676,8 +686,13 @@ contains
          qflx_irrig_col(c)     = 0._r8
 
          ! Insure water balance using qflx_qrgwl
-         qflx_qrgwl(c)     = forc_rain(t) + forc_snow(t) - qflx_evap_tot(p) - qflx_snwcp_ice(p) - &
+         if (.not. use_extrasnowlayers) then
+            qflx_qrgwl(c)     = forc_rain(t) + forc_snow(t) - qflx_evap_tot(p) - qflx_snwcp_ice(p) - &
               (endwb(c)-begwb(c))/dtime + qflx_floodg(g)
+         else ! qlfx_snwcp_ice(c) has been computed in routine SnowCapping
+            qflx_qrgwl(c)     = forc_rain(t) + forc_snow(t) - qflx_evap_tot(p) - qflx_snwcp_ice(c) - &
+              (endwb(c)-begwb(c))/dtime + qflx_floodg(g)
+         end if
          qflx_floodc(c)    = qflx_floodg(g)
          qflx_runoff(c)    = qflx_drain(c) + qflx_qrgwl(c)
          qflx_top_soil(c)  = qflx_prec_grnd_rain(p) + qflx_snomelt(c)

components/clm/src/biogeophys/SnowSnicarMod.F90
@@ -11,7 +11,7 @@ module SnowSnicarMod
   use shr_kind_mod    , only : r8 => shr_kind_r8
   use shr_sys_mod     , only : shr_sys_flush
   use shr_log_mod     , only : errMsg => shr_log_errMsg
-  use clm_varctl      , only : iulog
+  use clm_varctl      , only : iulog, use_extrasnowlayers
   use clm_varcon      , only : namec 
   use shr_const_mod   , only : SHR_CONST_RHOICE
   use abortutils      , only : endrun
@@ -1353,7 +1353,7 @@ contains
             !               RE-FREEZING
             !
             ! new snowfall [kg/m2]
-            if (do_capsnow(c_idx)) then
+            if (do_capsnow(c_idx) .and. .not. use_extrasnowlayers) then
                newsnow = max(0._r8, (qflx_snwcp_ice(c_idx)*dtime))
             else
                newsnow = max(0._r8, (qflx_snow_grnd_col(c_idx)*dtime))

components/clm/src/biogeophys/SoilFluxesMod.F90
@@ -10,7 +10,7 @@ module SoilFluxesMod
   use decompMod		, only : bounds_type
   use abortutils	, only : endrun
   use perf_mod		, only : t_startf, t_stopf
-  use clm_varctl	, only : iulog
+  use clm_varctl	, only : iulog, use_extrasnowlayers
   use clm_varpar	, only : nlevsno, nlevgrnd, nlevurb, max_patch_per_col
   use atm2lndType	, only : atm2lnd_type
   use CanopyStateType   , only : canopystate_type
@@ -350,7 +350,7 @@ contains
          ! This was moved in from Hydrology2 to keep all pft-level
          ! calculations out of Hydrology2
 
-         if (col_pp%snl(c) < 0 .and. do_capsnow(c)) then
+         if (col_pp%snl(c) < 0 .and. do_capsnow(c) .and. .not. use_extrasnowlayers) then
             qflx_snwcp_liq(p) = qflx_snwcp_liq(p)+frac_sno_eff(c)*qflx_dew_grnd(p)
             qflx_snwcp_ice(p) = qflx_snwcp_ice(p)+frac_sno_eff(c)*qflx_dew_snow(p)
          end if

rfiorella/test-E3SM

components/elm/src/biogeophys/BalanceCheckMod.F90
@@ -9,7 +9,7 @@ module BalanceCheckMod
   use shr_log_mod        , only : errMsg => shr_log_errMsg
   use decompMod          , only : bounds_type
   use abortutils         , only : endrun
-  use elm_varctl         , only : iulog, use_var_soil_thick
+  use elm_varctl         , only : iulog, use_var_soil_thick, use_extrasnowlayers
   use elm_varcon         , only : namep, namec
   use GetGlobalValuesMod , only : GetGlobalIndex
   use atm2lndType        , only : atm2lnd_type
@@ -444,37 +444,53 @@ contains
                      + qflx_snwcp_ice(c) + qflx_snwcp_liq(c) + qflx_sl_top_soil(c)
 
                 if (lun_pp%itype(l) == istdlak) then 
-                   if ( do_capsnow(c) ) then
+                   if ( do_capsnow(c) .and. .not. use_extrasnowlayers) then
                       snow_sources(c) = qflx_snow_grnd_col(c) &
                            + frac_sno_eff(c) * (qflx_dew_snow(c) + qflx_dew_grnd(c) ) 
 
                       snow_sinks(c)   = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c) ) &
                            + (qflx_snwcp_ice(c) + qflx_snwcp_liq(c) - qflx_prec_grnd(c))  &
                            + qflx_snow_melt(c)  + qflx_sl_top_soil(c)
-                   else
+                   else if (.not. use_extrasnowlayers) then
                       snow_sources(c) = qflx_snow_grnd_col(c) &
                            + frac_sno_eff(c) * (qflx_rain_grnd_col(c) &
                            +  qflx_dew_snow(c) + qflx_dew_grnd(c) ) 
 
                       snow_sinks(c)  = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c) ) &
                            + qflx_snow_melt(c)  + qflx_sl_top_soil(c)
+                   else if (use_extrasnowlayers) then !++ams i.e., firn model
+                      snow_sources(c) = qflx_snow_grnd_col(c) &
+                           + frac_sno_eff(c) * (qflx_rain_grnd_col(c) &
+                           +  qflx_dew_snow(c) + qflx_dew_grnd(c) ) 
+
+                      snow_sinks(c)  = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c) ) &
+                           + qflx_snwcp_ice(c) + qflx_snwcp_liq(c) &
+                           + qflx_snow_melt(c) + qflx_sl_top_soil(c)
                    endif
                 endif
 
                 if (lun_pp%itype(l) == istsoil .or. lun_pp%itype(l) == istcrop .or. lun_pp%itype(l) == istwet ) then
-                   if ( do_capsnow(c) ) then
+                   if ( do_capsnow(c) .and. .not. use_extrasnowlayers) then
                       snow_sources(c) = frac_sno_eff(c) * (qflx_dew_snow(c) + qflx_dew_grnd(c) ) &
                            + qflx_h2osfc_to_ice(c) + qflx_prec_grnd(c)
 
                       snow_sinks(c) = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c)) &
                            + qflx_snwcp_ice(c) + qflx_snwcp_liq(c) &
                            + qflx_snow_melt(c) + qflx_sl_top_soil(c)
-                   else
+                   else if (.not. use_extrasnowlayers) then
                       snow_sources(c) = (qflx_snow_grnd_col(c) - qflx_snow_h2osfc(c) ) &
                            + frac_sno_eff(c) * (qflx_rain_grnd_col(c) &
                            +  qflx_dew_snow(c) + qflx_dew_grnd(c) ) + qflx_h2osfc_to_ice(c)
 
                       snow_sinks(c) = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c)) &
+                           + qflx_snow_melt(c) + qflx_sl_top_soil(c)
+                   else if (use_extrasnowlayers) then !++ams i.e., firn model
+                      snow_sources(c) = (qflx_snow_grnd_col(c) - qflx_snow_h2osfc(c) ) &
+                           + frac_sno_eff(c) * (qflx_rain_grnd_col(c) &
+                           +  qflx_dew_snow(c) + qflx_dew_grnd(c) ) + qflx_h2osfc_to_ice(c)
+
+                      snow_sinks(c) = frac_sno_eff(c) * (qflx_sub_snow(c) + qflx_evap_grnd(c)) &
+                           + qflx_snwcp_ice(c) + qflx_snwcp_liq(c) &
                            + qflx_snow_melt(c) + qflx_sl_top_soil(c)
                    endif
                 endif

components/elm/src/biogeophys/HydrologyNoDrainageMod.F90
@@ -7,7 +7,7 @@ Module HydrologyNoDrainageMod
   use shr_kind_mod      , only : r8 => shr_kind_r8
   use shr_log_mod       , only : errMsg => shr_log_errMsg
   use decompMod         , only : bounds_type
-  use elm_varctl        , only : iulog, use_vichydro
+  use elm_varctl        , only : iulog, use_vichydro, use_extrasnowlayers
   use elm_varcon        , only : e_ice, denh2o, denice, rpi, spval
   use atm2lndType       , only : atm2lnd_type
   use AerosolType       , only : aerosol_type
@@ -70,7 +70,7 @@ contains
     use elm_varctl           , only : use_cn, use_betr, use_fates, use_pflotran, pf_hmode
     use elm_varpar           , only : nlevgrnd, nlevsno, nlevsoi, nlevurb
     use clm_time_manager     , only : get_step_size, get_nstep
-    use SnowHydrologyMod     , only : SnowCompaction, CombineSnowLayers, DivideSnowLayers
+    use SnowHydrologyMod     , only : SnowCompaction, CombineSnowLayers, DivideSnowLayers, SnowCapping
     use SnowHydrologyMod     , only : SnowWater, BuildSnowFilter 
     use SoilHydrologyMod     , only : ELMVICMap, SurfaceRunoff, Infiltration, WaterTable
     use SoilWaterMovementMod , only : SoilWater 
@@ -297,6 +297,12 @@ contains
          !Jinyun Tang, Feb 4, 2015
          call ep_betr%CalcDewSubFlux(bounds, col_pp, num_hydrologyc, filter_hydrologyc)
       endif           
+      
+      if (use_extrasnowlayers) then
+         call SnowCapping(bounds, num_nolakec, filter_nolakec, num_snowc, filter_snowc, &
+                          aerosol_vars, waterflux_vars, waterstate_vars)
+      end if
+      
       ! Natural compaction and metamorphosis.
       call SnowCompaction(bounds, num_snowc, filter_snowc, &
            temperature_vars, waterstate_vars, top_as)

components/elm/src/biogeophys/LakeFluxesMod.F90
@@ -51,6 +51,7 @@ contains
     use elm_varcon          , only : hvap, hsub, hfus, cpair, cpliq, tkwat, tkice, tkair
     use elm_varcon          , only : sb, vkc, grav, denh2o, tfrz, spval, zsno
     use elm_varctl          , only : use_lch4
+    use elm_varctl          , only : use_lch4, use_extrasnowlayers
     use LakeCon             , only : betavis, z0frzlake, tdmax, emg_lake
     use LakeCon             , only : lake_use_old_fcrit_minz0
     use LakeCon             , only : minz0lake, cur0, cus, curm, fcrit
@@ -634,10 +635,12 @@ contains
          qflx_dirct_rain(p) = 0._r8
          qflx_leafdrip(p) = 0._r8
 
-         ! Because they will be used in pft2col initialize here.
-         ! This will be overwritten in LakeHydrology
-         qflx_snwcp_ice(p) = 0._r8
-         qflx_snwcp_liq(p) = 0._r8
+         if (.not. use_extrasnowlayers) then
+            ! Because they will be used in pft2col initialize here.
+            ! This will be overwritten in LakeHydrology
+            qflx_snwcp_ice(p) = 0._r8
+            qflx_snwcp_liq(p) = 0._r8
+         end if
 
       end do
 

components/elm/src/biogeophys/LakeHydrologyMod.F90
@@ -73,10 +73,10 @@ contains
     ! !USES:
     use elm_varcon      , only : denh2o, denice, spval, hfus, tfrz, cpliq, cpice
     use elm_varpar      , only : nlevsno, nlevgrnd, nlevsoi
-    use elm_varctl      , only : iulog
+    use elm_varctl      , only : iulog, use_extrasnowlayers
     use clm_time_manager, only : get_step_size
     use SnowHydrologyMod, only : SnowCompaction, CombineSnowLayers, SnowWater, BuildSnowFilter
-    use SnowHydrologyMod, only : DivideSnowLayers
+    use SnowHydrologyMod, only : DivideSnowLayers, SnowCapping
     use LakeCon         , only : lsadz
     !
     ! !ARGUMENTS:
@@ -247,15 +247,19 @@ contains
 
          qflx_dirct_rain(p) = 0._r8
          qflx_leafdrip(p) = 0._r8
-
-         if (do_capsnow(c)) then
-            qflx_snwcp_ice(p) = qflx_prec_grnd_snow(p)
-            qflx_snwcp_liq(p) = qflx_prec_grnd_rain(p)
-            qflx_snow_grnd_patch(p) = 0._r8
-            qflx_rain_grnd(p) = 0._r8
+         if (.not. use_extrasnowlayers) then
+            if (do_capsnow(c)) then
+               qflx_snwcp_ice(p) = qflx_prec_grnd_snow(p)
+               qflx_snwcp_liq(p) = qflx_prec_grnd_rain(p)
+               qflx_snow_grnd_patch(p) = 0._r8
+               qflx_rain_grnd(p) = 0._r8
+            else
+               qflx_snwcp_ice(p) = 0._r8
+               qflx_snwcp_liq(p) = 0._r8
+               qflx_snow_grnd_patch(p) = qflx_prec_grnd_snow(p)           ! ice onto ground (mm/s)
+               qflx_rain_grnd(p)     = qflx_prec_grnd_rain(p)           ! liquid water onto ground (mm/s)
+            end if
          else
-            qflx_snwcp_ice(p) = 0._r8
-            qflx_snwcp_liq(p) = 0._r8
             qflx_snow_grnd_patch(p) = qflx_prec_grnd_snow(p)           ! ice onto ground (mm/s)
             qflx_rain_grnd(p)     = qflx_prec_grnd_rain(p)           ! liquid water onto ground (mm/s)
          end if
@@ -275,7 +279,7 @@ contains
          ! U.S.Department of Agriculture Forest Service, Project F,
          ! Progress Rep. 1, Alta Avalanche Study Center:Snow Layer Densification.
 
-         if (do_capsnow(c)) then
+         if (do_capsnow(c) .and. .not. use_extrasnowlayers) then
             dz_snowf = 0._r8
          else
             if (forc_t(t) > tfrz + 2._r8) then
@@ -368,7 +372,7 @@ contains
             ! Update the pft-level qflx_snowcap
             ! This was moved in from Hydrology2 to keep all pft-level
             ! calculations out of Hydrology2
-            if (do_capsnow(c)) then
+            if (do_capsnow(c) .and. .not. use_extrasnowlayers) then
                qflx_snwcp_ice(p) = qflx_snwcp_ice(p) + qflx_dew_snow(p) 
                qflx_snwcp_liq(p) = qflx_snwcp_liq(p) + qflx_dew_grnd(p)
             end if
@@ -390,7 +394,7 @@ contains
             ! Update snow pack for dew & sub.
 
             h2osno_temp = h2osno(c)
-            if (do_capsnow(c)) then
+            if (do_capsnow(c) .and. .not. use_extrasnowlayers) then
                h2osno(c) = h2osno(c) - qflx_sub_snow(p)*dtime
                qflx_snwcp_ice(p) = qflx_snwcp_ice(p) + qflx_dew_snow(p) 
                qflx_snwcp_liq(p) = qflx_snwcp_liq(p) + qflx_dew_grnd(p)
@@ -406,9 +410,10 @@ contains
             h2osno(c) = max(h2osno(c), 0._r8)
          end if
 
-         qflx_snwcp_ice_col(c) = qflx_snwcp_ice(p)
-         qflx_snwcp_liq_col(c) = qflx_snwcp_liq(p)
-
+         if (.not. use_extrasnowlayers) then
+            qflx_snwcp_ice_col(c) = qflx_snwcp_ice(p)
+            qflx_snwcp_liq_col(c) = qflx_snwcp_liq(p)
+         end if
 
       end do
 
@@ -452,6 +457,11 @@ contains
       call SnowWater(bounds, &
            num_shlakesnowc, filter_shlakesnowc, num_shlakenosnowc, filter_shlakenosnowc, &
            atm2lnd_vars, waterflux_vars, waterstate_vars, aerosol_vars)
+           
+      if (use_extrasnowlayers) then
+         call SnowCapping(bounds, num_lakec, filter_lakec, num_shlakesnowc, filter_shlakesnowc, &
+                          aerosol_vars, waterflux_vars, waterstate_vars)
+      end if
 
       ! Determine soil hydrology
       ! Here this consists only of making sure that soil is saturated even as it melts and
@@ -676,8 +686,13 @@ contains
          qflx_irrig_col(c)     = 0._r8
 
          ! Insure water balance using qflx_qrgwl
-         qflx_qrgwl(c)     = forc_rain(t) + forc_snow(t) - qflx_evap_tot(p) - qflx_snwcp_ice(p) - &
+         if (.not. use_extrasnowlayers) then
+            qflx_qrgwl(c)     = forc_rain(t) + forc_snow(t) - qflx_evap_tot(p) - qflx_snwcp_ice(p) - &
               (endwb(c)-begwb(c))/dtime + qflx_floodg(g)
+         else ! qlfx_snwcp_ice(c) has been computed in routine SnowCapping
+            qflx_qrgwl(c)     = forc_rain(t) + forc_snow(t) - qflx_evap_tot(p) - qflx_snwcp_ice(c) - &
+              (endwb(c)-begwb(c))/dtime + qflx_floodg(g)
+         end if
          qflx_floodc(c)    = qflx_floodg(g)
          qflx_runoff(c)    = qflx_drain(c) + qflx_qrgwl(c)
          qflx_top_soil(c)  = qflx_prec_grnd_rain(p) + qflx_snomelt(c)

components/elm/src/biogeophys/SnowSnicarMod.F90
@@ -11,7 +11,7 @@ module SnowSnicarMod
   use shr_kind_mod    , only : r8 => shr_kind_r8
   use shr_sys_mod     , only : shr_sys_flush
   use shr_log_mod     , only : errMsg => shr_log_errMsg
-  use elm_varctl      , only : iulog
+  use elm_varctl      , only : iulog, use_extrasnowlayers
   use elm_varcon      , only : namec 
   use shr_const_mod   , only : SHR_CONST_RHOICE
   use abortutils      , only : endrun
@@ -1353,7 +1353,7 @@ contains
             !               RE-FREEZING
             !
             ! new snowfall [kg/m2]
-            if (do_capsnow(c_idx)) then
+            if (do_capsnow(c_idx) .and. .not. use_extrasnowlayers) then
                newsnow = max(0._r8, (qflx_snwcp_ice(c_idx)*dtime))
             else
                newsnow = max(0._r8, (qflx_snow_grnd_col(c_idx)*dtime))

components/elm/src/biogeophys/SoilFluxesMod.F90
@@ -10,7 +10,7 @@ module SoilFluxesMod
   use decompMod		, only : bounds_type
   use abortutils	, only : endrun
   use perf_mod		, only : t_startf, t_stopf
-  use elm_varctl	, only : iulog
+  use elm_varctl	, only : iulog, use_extrasnowlayers
   use elm_varpar	, only : nlevsno, nlevgrnd, nlevurb, max_patch_per_col
   use atm2lndType	, only : atm2lnd_type
   use CanopyStateType   , only : canopystate_type
@@ -350,7 +350,7 @@ contains
          ! This was moved in from Hydrology2 to keep all pft-level
          ! calculations out of Hydrology2
 
-         if (col_pp%snl(c) < 0 .and. do_capsnow(c)) then
+         if (col_pp%snl(c) < 0 .and. do_capsnow(c) .and. .not. use_extrasnowlayers) then
             qflx_snwcp_liq(p) = qflx_snwcp_liq(p)+frac_sno_eff(c)*qflx_dew_grnd(p)
             qflx_snwcp_ice(p) = qflx_snwcp_ice(p)+frac_sno_eff(c)*qflx_dew_snow(p)
          end if