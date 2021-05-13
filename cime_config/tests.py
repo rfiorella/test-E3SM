E3SM-Project/E3SM

Adds test for variable soil with CTC-RD

cime_config/tests.py
@@ -24,6 +24,7 @@ _TESTS = {
             "ERS_Ld20.f45_f45.ICLM45ED.clm-fates",
             "ERS.f19_g16.I1850CLM45.clm-betr",
             "ERS.f19_g16.I1850CLM45.clm-vst",
+            "ERS.f09_g16.I1850GSWCNPRDCTCBC.clm-vstrd",
             "ERS.f09_g16.I1850CLM45CN.clm-bgcinterface",
             "ERS.ne11_oQU240.I20TRCLM45",
             "ERS.f19_g16.I1850CNRDCTCBC.clm-rd",

rfiorella/test-E3SM

cime_config/tests.py
@@ -24,6 +24,7 @@ _TESTS = {
             "ERS_Ld20.f45_f45.IELMFATES.elm-fates",
             "ERS.f19_g16.I1850ELM.elm-betr",
             "ERS.f19_g16.I1850ELM.elm-vst",
+            "ERS.f09_g16.I1850GSWCNPRDCTCBC.elm-vstrd",
             "ERS.f09_g16.I1850ELMCN.elm-bgcinterface",
             "ERS.ne11_oQU240.I20TRELM",
             "ERS.f19_g16.I1850CNRDCTCBC.elm-rd",