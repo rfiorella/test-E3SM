wagmanbe/E3SM

Call C++ versions of run_sw and run_lw and compare with F90

components/eam/src/physics/crm/rrtmgp/radiation.F90
@@ -1960,6 +1960,7 @@ contains
       real(r8), dimension(ncol,pver,nswgpts) :: cld_tau_gpt_day, cld_ssa_gpt_day, cld_asm_gpt_day
       real(r8), dimension(ncol,pver,nswbands) :: aer_tau_bnd_day, aer_ssa_bnd_day, aer_asm_bnd_day
       type(fluxes_t) :: fluxes_allsky_day, fluxes_clrsky_day
+      type(fluxes_t) :: fluxes_allsky_cxx, fluxes_clrsky_cxx
 
       real(r8), dimension(size(cld_tau_gpt,1),size(cld_tau_gpt,2)+1,size(cld_tau_gpt,3)) :: cld_tau_gpt_rad
       real(r8), dimension(size(cld_ssa_gpt,1),size(cld_ssa_gpt,2)+1,size(cld_ssa_gpt,3)) :: cld_ssa_gpt_rad
@@ -2060,28 +2061,7 @@ contains
       gas_vmr_rad(:,1:nday,ktop:kbot) = gas_vmr_day(:,1:nday,1:pver)
 
       ! Do shortwave radiative transfer calculations
-      call t_startf('rad_calculations_sw')
-!     call rrtmgp_run_sw( &
-!        size(active_gases), nday, nlev_rad, &
-!        gas_names, gas_vmr_day, &
-!        pmid_day(1:nday,1:nlev_rad), &
-!        tmid_day(1:nday,1:nlev_rad), &
-!        pint_day(1:nday,1:nlev_rad+1), &
-!        coszrs_day(1:nday), &
-!        albedo_dir_day(1:nswbands,1:nday), &
-!        albedo_dif_day(1:nswbands,1:nday), &
-!        cld_tau_gpt_day(1:nday,1:pver,1:nswgpts), cld_ssa_gpt_day(1:nday,1:pver,1:nswgpts), cld_asm_gpt_day(1:nday,1:pver,1:nswgpts), &
-!        aer_tau_bnd_day(1:nday,1:pver,1:nswbands), aer_ssa_bnd_day(1:nday,1:pver,1:nswbands), aer_asm_bnd_day(1:nday,1:pver,1:nswbands), &
-!        fluxes_allsky_day%flux_up, fluxes_allsky_day%flux_dn, fluxes_allsky_day%flux_net, &
-!        fluxes_allsky_day%flux_dn_dir, &
-!        fluxes_allsky_day%bnd_flux_up, fluxes_allsky_day%bnd_flux_dn, fluxes_allsky_day%bnd_flux_net, &
-!        fluxes_allsky_day%bnd_flux_dn_dir, &
-!        fluxes_clrsky_day%flux_up, fluxes_clrsky_day%flux_dn, fluxes_clrsky_day%flux_net, &
-!        fluxes_clrsky_day%flux_dn_dir, &
-!        fluxes_clrsky_day%bnd_flux_up, fluxes_clrsky_day%bnd_flux_dn, fluxes_clrsky_day%bnd_flux_net, &
-!        fluxes_clrsky_day%bnd_flux_dn_dir, &
-!        tsi_scaling &
-!     )
+      call t_startf('rad_rrtmgp_run_sw')
       call rrtmgp_run_sw( &
          size(active_gases), nday, nlev_rad, &
          gas_names, gas_vmr_rad(:,1:nday,1:nlev_rad), &
@@ -2099,8 +2079,39 @@ contains
          fluxes_clrsky_day%bnd_flux_up, fluxes_clrsky_day%bnd_flux_dn, fluxes_clrsky_day%bnd_flux_net, fluxes_clrsky_day%bnd_flux_dn_dir, &
          tsi_scaling &
       )
-
-      call t_stopf('rad_calculations_sw')
+      call t_stopf('rad_rrtmgp_run_sw')
+      call t_startf('rad_rrtmgpxx_run_sw')
+      call initialize_fluxes(nday, nlev_rad+1, nswbands, fluxes_allsky_cxx, do_direct=.true.)
+      call initialize_fluxes(nday, nlev_rad+1, nswbands, fluxes_clrsky_cxx, do_direct=.true.)
+      call rrtmgpxx_run_sw( &
+         size(active_gases), nday, nlev_rad, &
+         c_strarr(active_gases, active_gases_c), gas_vmr_rad(:,1:nday,1:nlev_rad), &
+         pmid_day(1:nday,1:nlev_rad), &
+         tmid_day(1:nday,1:nlev_rad), &
+         pint_day(1:nday,1:nlev_rad+1), &
+         coszrs_day(1:nday), &
+         albedo_dir_day(1:nswbands,1:nday), &
+         albedo_dif_day(1:nswbands,1:nday), &
+         cld_tau_gpt_rad(1:nday,1:nlev_rad,1:nswgpts), cld_ssa_gpt_rad(1:nday,1:nlev_rad,1:nswgpts), cld_asm_gpt_rad(1:nday,1:nlev_rad,1:nswgpts), &
+         aer_tau_bnd_rad(1:nday,1:nlev_rad,1:nswbands), aer_ssa_bnd_rad(1:nday,1:nlev_rad,1:nswbands), aer_asm_bnd_rad(1:nday,1:nlev_rad,1:nswbands), &
+         fluxes_allsky_cxx%flux_up    , fluxes_allsky_cxx%flux_dn    , fluxes_allsky_cxx%flux_net    , fluxes_allsky_cxx%flux_dn_dir    , &
+         fluxes_allsky_cxx%bnd_flux_up, fluxes_allsky_cxx%bnd_flux_dn, fluxes_allsky_cxx%bnd_flux_net, fluxes_allsky_cxx%bnd_flux_dn_dir, &
+         fluxes_clrsky_cxx%flux_up    , fluxes_clrsky_cxx%flux_dn    , fluxes_clrsky_cxx%flux_net    , fluxes_clrsky_cxx%flux_dn_dir    , &
+         fluxes_clrsky_cxx%bnd_flux_up, fluxes_clrsky_cxx%bnd_flux_dn, fluxes_clrsky_cxx%bnd_flux_net, fluxes_clrsky_cxx%bnd_flux_dn_dir, &
+         tsi_scaling &
+      )
+      call t_stopf('rad_rrtmgpxx_run_sw')
+      ! Check fluxes
+      if (.true.) then
+         call assert(all(abs(fluxes_allsky_cxx%flux_up - fluxes_allsky_day%flux_up) < 1e-5), 'F90 and CXX allsky_flux_up differs.')
+         call assert(all(abs(fluxes_allsky_cxx%flux_dn - fluxes_allsky_day%flux_dn) < 1e-5), 'F90 and CXX allsky_flux_dn differs.')
+         call assert(all(abs(fluxes_allsky_cxx%flux_net - fluxes_allsky_day%flux_net) < 1e-5), 'F90 and CXX allsky_flux_net differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_up - fluxes_clrsky_day%flux_up) < 1e-5), 'F90 and CXX clrsky_flux_up differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_dn - fluxes_clrsky_day%flux_dn) < 1e-5), 'F90 and CXX clrsky_flux_dn differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_net - fluxes_clrsky_day%flux_net) < 1e-5), 'F90 and CXX clrsky_flux_net differs.')
+      end if
+      call free_fluxes(fluxes_allsky_cxx)
+      call free_fluxes(fluxes_clrsky_cxx)
 
       ! Expand fluxes from daytime-only arrays to full chunk arrays
       call t_startf('rad_expand_fluxes_sw')
@@ -2146,6 +2157,7 @@ contains
       real(r8), intent(in) :: pmid(:,:), tmid(:,:), pint(:,:), tint(:,:)
       real(r8), intent(in) :: cld_tau_gpt(:,:,:), aer_tau_bnd(:,:,:)
       type(fluxes_t), intent(inout) :: fluxes_allsky, fluxes_clrsky
+      type(fluxes_t) :: fluxes_allsky_cxx, fluxes_clrsky_cxx
 
       integer :: ncol, nlev, igas
 
@@ -2179,6 +2191,33 @@ contains
             fluxes_clrsky%bnd_flux_up, fluxes_clrsky%bnd_flux_dn, fluxes_clrsky%bnd_flux_net &
             )
       call t_stopf('rrtmgp_run_lw')
+      call t_startf('rrtmgpxx_run_lw')
+      ! Try calling C++ version
+      call initialize_fluxes(ncol, nlev_rad+1, nlwbands, fluxes_allsky_cxx)
+      call initialize_fluxes(ncol, nlev_rad+1, nlwbands, fluxes_clrsky_cxx)
+      call rrtmgpxx_run_lw( &
+         size(active_gases), ncol, nlev_rad, &
+         c_strarr(active_gases, active_gases_c), gas_vmr_rad(:,1:ncol,:), &
+         pmid(1:ncol,1:nlev_rad), tmid(1:ncol,1:nlev_rad), pint(1:ncol,1:nlev_rad+1), tint(1:ncol,1:nlev_rad+1), &
+         surface_emissivity(1:nlwbands,1:ncol), &
+         cld_tau_gpt_rad(1:ncol,:,:)  , aer_tau_bnd_rad(1:ncol,:,:)  , &
+         fluxes_allsky_cxx%flux_up    , fluxes_allsky_cxx%flux_dn    , fluxes_allsky_cxx%flux_net    , &
+         fluxes_allsky_cxx%bnd_flux_up, fluxes_allsky_cxx%bnd_flux_dn, fluxes_allsky_cxx%bnd_flux_net, &
+         fluxes_clrsky_cxx%flux_up    , fluxes_clrsky_cxx%flux_dn    , fluxes_clrsky_cxx%flux_net    , &
+         fluxes_clrsky_cxx%bnd_flux_up, fluxes_clrsky_cxx%bnd_flux_dn, fluxes_clrsky_cxx%bnd_flux_net  &
+         )
+      call t_stopf('rrtmgpxx_run_lw')
+      ! Check fluxes
+      if (.true.) then
+         call assert(all(abs(fluxes_allsky_cxx%flux_up - fluxes_allsky%flux_up) < 1e-5), 'F90 and CXX allsky_flux_up differs.')
+         call assert(all(abs(fluxes_allsky_cxx%flux_dn - fluxes_allsky%flux_dn) < 1e-5), 'F90 and CXX allsky_flux_dn differs.')
+         call assert(all(abs(fluxes_allsky_cxx%flux_net - fluxes_allsky%flux_net) < 1e-5), 'F90 and CXX allsky_flux_net differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_up - fluxes_clrsky%flux_up) < 1e-5), 'F90 and CXX clrsky_flux_up differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_dn - fluxes_clrsky%flux_dn) < 1e-5), 'F90 and CXX clrsky_flux_dn differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_net - fluxes_clrsky%flux_net) < 1e-5), 'F90 and CXX clrsky_flux_net differs.')
+      end if
+      call free_fluxes(fluxes_allsky_cxx)
+      call free_fluxes(fluxes_clrsky_cxx)
 
    end subroutine radiation_driver_lw
 

rfiorella/test-E3SM

components/eam/src/physics/crm/rrtmgp/radiation.F90
@@ -1960,6 +1960,7 @@ contains
       real(r8), dimension(ncol,pver,nswgpts) :: cld_tau_gpt_day, cld_ssa_gpt_day, cld_asm_gpt_day
       real(r8), dimension(ncol,pver,nswbands) :: aer_tau_bnd_day, aer_ssa_bnd_day, aer_asm_bnd_day
       type(fluxes_t) :: fluxes_allsky_day, fluxes_clrsky_day
+      type(fluxes_t) :: fluxes_allsky_cxx, fluxes_clrsky_cxx
 
       real(r8), dimension(size(cld_tau_gpt,1),size(cld_tau_gpt,2)+1,size(cld_tau_gpt,3)) :: cld_tau_gpt_rad
       real(r8), dimension(size(cld_ssa_gpt,1),size(cld_ssa_gpt,2)+1,size(cld_ssa_gpt,3)) :: cld_ssa_gpt_rad
@@ -2060,28 +2061,7 @@ contains
       gas_vmr_rad(:,1:nday,ktop:kbot) = gas_vmr_day(:,1:nday,1:pver)
 
       ! Do shortwave radiative transfer calculations
-      call t_startf('rad_calculations_sw')
-!     call rrtmgp_run_sw( &
-!        size(active_gases), nday, nlev_rad, &
-!        gas_names, gas_vmr_day, &
-!        pmid_day(1:nday,1:nlev_rad), &
-!        tmid_day(1:nday,1:nlev_rad), &
-!        pint_day(1:nday,1:nlev_rad+1), &
-!        coszrs_day(1:nday), &
-!        albedo_dir_day(1:nswbands,1:nday), &
-!        albedo_dif_day(1:nswbands,1:nday), &
-!        cld_tau_gpt_day(1:nday,1:pver,1:nswgpts), cld_ssa_gpt_day(1:nday,1:pver,1:nswgpts), cld_asm_gpt_day(1:nday,1:pver,1:nswgpts), &
-!        aer_tau_bnd_day(1:nday,1:pver,1:nswbands), aer_ssa_bnd_day(1:nday,1:pver,1:nswbands), aer_asm_bnd_day(1:nday,1:pver,1:nswbands), &
-!        fluxes_allsky_day%flux_up, fluxes_allsky_day%flux_dn, fluxes_allsky_day%flux_net, &
-!        fluxes_allsky_day%flux_dn_dir, &
-!        fluxes_allsky_day%bnd_flux_up, fluxes_allsky_day%bnd_flux_dn, fluxes_allsky_day%bnd_flux_net, &
-!        fluxes_allsky_day%bnd_flux_dn_dir, &
-!        fluxes_clrsky_day%flux_up, fluxes_clrsky_day%flux_dn, fluxes_clrsky_day%flux_net, &
-!        fluxes_clrsky_day%flux_dn_dir, &
-!        fluxes_clrsky_day%bnd_flux_up, fluxes_clrsky_day%bnd_flux_dn, fluxes_clrsky_day%bnd_flux_net, &
-!        fluxes_clrsky_day%bnd_flux_dn_dir, &
-!        tsi_scaling &
-!     )
+      call t_startf('rad_rrtmgp_run_sw')
       call rrtmgp_run_sw( &
          size(active_gases), nday, nlev_rad, &
          gas_names, gas_vmr_rad(:,1:nday,1:nlev_rad), &
@@ -2099,8 +2079,39 @@ contains
          fluxes_clrsky_day%bnd_flux_up, fluxes_clrsky_day%bnd_flux_dn, fluxes_clrsky_day%bnd_flux_net, fluxes_clrsky_day%bnd_flux_dn_dir, &
          tsi_scaling &
       )
-
-      call t_stopf('rad_calculations_sw')
+      call t_stopf('rad_rrtmgp_run_sw')
+      call t_startf('rad_rrtmgpxx_run_sw')
+      call initialize_fluxes(nday, nlev_rad+1, nswbands, fluxes_allsky_cxx, do_direct=.true.)
+      call initialize_fluxes(nday, nlev_rad+1, nswbands, fluxes_clrsky_cxx, do_direct=.true.)
+      call rrtmgpxx_run_sw( &
+         size(active_gases), nday, nlev_rad, &
+         c_strarr(active_gases, active_gases_c), gas_vmr_rad(:,1:nday,1:nlev_rad), &
+         pmid_day(1:nday,1:nlev_rad), &
+         tmid_day(1:nday,1:nlev_rad), &
+         pint_day(1:nday,1:nlev_rad+1), &
+         coszrs_day(1:nday), &
+         albedo_dir_day(1:nswbands,1:nday), &
+         albedo_dif_day(1:nswbands,1:nday), &
+         cld_tau_gpt_rad(1:nday,1:nlev_rad,1:nswgpts), cld_ssa_gpt_rad(1:nday,1:nlev_rad,1:nswgpts), cld_asm_gpt_rad(1:nday,1:nlev_rad,1:nswgpts), &
+         aer_tau_bnd_rad(1:nday,1:nlev_rad,1:nswbands), aer_ssa_bnd_rad(1:nday,1:nlev_rad,1:nswbands), aer_asm_bnd_rad(1:nday,1:nlev_rad,1:nswbands), &
+         fluxes_allsky_cxx%flux_up    , fluxes_allsky_cxx%flux_dn    , fluxes_allsky_cxx%flux_net    , fluxes_allsky_cxx%flux_dn_dir    , &
+         fluxes_allsky_cxx%bnd_flux_up, fluxes_allsky_cxx%bnd_flux_dn, fluxes_allsky_cxx%bnd_flux_net, fluxes_allsky_cxx%bnd_flux_dn_dir, &
+         fluxes_clrsky_cxx%flux_up    , fluxes_clrsky_cxx%flux_dn    , fluxes_clrsky_cxx%flux_net    , fluxes_clrsky_cxx%flux_dn_dir    , &
+         fluxes_clrsky_cxx%bnd_flux_up, fluxes_clrsky_cxx%bnd_flux_dn, fluxes_clrsky_cxx%bnd_flux_net, fluxes_clrsky_cxx%bnd_flux_dn_dir, &
+         tsi_scaling &
+      )
+      call t_stopf('rad_rrtmgpxx_run_sw')
+      ! Check fluxes
+      if (.true.) then
+         call assert(all(abs(fluxes_allsky_cxx%flux_up - fluxes_allsky_day%flux_up) < 1e-5), 'F90 and CXX allsky_flux_up differs.')
+         call assert(all(abs(fluxes_allsky_cxx%flux_dn - fluxes_allsky_day%flux_dn) < 1e-5), 'F90 and CXX allsky_flux_dn differs.')
+         call assert(all(abs(fluxes_allsky_cxx%flux_net - fluxes_allsky_day%flux_net) < 1e-5), 'F90 and CXX allsky_flux_net differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_up - fluxes_clrsky_day%flux_up) < 1e-5), 'F90 and CXX clrsky_flux_up differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_dn - fluxes_clrsky_day%flux_dn) < 1e-5), 'F90 and CXX clrsky_flux_dn differs.')
+         call assert(all(abs(fluxes_clrsky_cxx%flux_net - fluxes_clrsky_day%flux_net) < 1e-5), 'F90 and CXX clrsky_flux_net differs.')
+      end if
+      call free_fluxes(fluxes_allsky_cxx)
+      call free_fluxes(fluxes_clrsky_cxx)
 
       ! Expand fluxes from daytime-only arrays to full chunk arrays
       call t_startf('rad_expand_fluxes_sw')
@@ -2166,20 +2177,19 @@ contains
       gas_vmr_rad(:,1:ncol,ktop:kbot) = gas_vmr(:,1:ncol,:)
 
       ! Compute fluxes
-      call t_startf('rrtmgp_run_lw')
-      call rrtmgp_run_lw( &
-            size(active_gases), ncol, nlev_rad, &
-            gas_names, gas_vmr_rad(:,1:ncol,1:nlev_rad), &
-            surface_emissivity(1:nlwbands,1:ncol), &
-            pmid(1:ncol,:), tmid(1:ncol,:), pint(1:ncol,:), tint(1:ncol,:), &
-            cld_tau_gpt_rad(1:ncol,:,:), aer_tau_bnd_rad(1:ncol,:,:), &
-            fluxes_allsky%flux_up, fluxes_allsky%flux_dn, fluxes_allsky%flux_net, &
-            fluxes_allsky%bnd_flux_up, fluxes_allsky%bnd_flux_dn, fluxes_allsky%bnd_flux_net, &
-            fluxes_clrsky%flux_up, fluxes_clrsky%flux_dn, fluxes_clrsky%flux_net, &
-            fluxes_clrsky%bnd_flux_up, fluxes_clrsky%bnd_flux_dn, fluxes_clrsky%bnd_flux_net &
-            )
-      call t_stopf('rrtmgp_run_lw')
-
+      call t_startf('rrtmgpxx_run_lw')
+      call rrtmgpxx_run_lw( &
+         size(active_gases), ncol, nlev_rad, &
+         c_strarr(active_gases, active_gases_c), gas_vmr_rad(:,1:ncol,:), &
+         pmid(1:ncol,1:nlev_rad), tmid(1:ncol,1:nlev_rad), pint(1:ncol,1:nlev_rad+1), tint(1:ncol,1:nlev_rad+1), &
+         surface_emissivity(1:nlwbands,1:ncol), &
+         cld_tau_gpt_rad(1:ncol,:,:)  , aer_tau_bnd_rad(1:ncol,:,:)  , &
+         fluxes_allsky%flux_up    , fluxes_allsky%flux_dn    , fluxes_allsky%flux_net    , &
+         fluxes_allsky%bnd_flux_up, fluxes_allsky%bnd_flux_dn, fluxes_allsky%bnd_flux_net, &
+         fluxes_clrsky%flux_up    , fluxes_clrsky%flux_dn    , fluxes_clrsky%flux_net    , &
+         fluxes_clrsky%bnd_flux_up, fluxes_clrsky%bnd_flux_dn, fluxes_clrsky%bnd_flux_net  &
+         )
+      call t_stopf('rrtmgpxx_run_lw')
    end subroutine radiation_driver_lw
 
    !----------------------------------------------------------------------------