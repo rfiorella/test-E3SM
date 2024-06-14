ORNLngee/NGEE-Arctic-E3SM_lanl

Update snow burial of vegetation parameterization
Enables two major changes to the parameterization that calculates LAI
burial by snow: a) pulls stocking density and taper parameters that
influence vegetation height from hard-wired values in
biogeochem/VegStructUpdateMod.F90 to allow to be specified on the
surface file (per main/pftvarcon) and b) updates the LAI burial by snow
rate from Wang and Zeng, 2007 (linear burial) to form proposed
by Liston and Hiemstra, 2011 (linear or non-linear) burial. The Liston
and Heimstra (2011) formulation introduces two new parameters:
1) bendresist, which specifies how resistant branches are to bending
under snow loading (1 = no bending, lim->0 branches bend very rapidly)
and 2) vegshape, which specifies how the shape of the vegetation crown
affects how rapidly it is buried by snow (1 = paraboloid; 2 = hemispheroid).
These new parameters can also be specified on surface file.

components/elm/src/data_types/VegetationPropertiesType.F90
@@ -115,7 +115,7 @@ module VegetationPropertiesType
      real(r8), pointer :: lamda_ptase              => null()! critical value that incur biochemical production
      real(r8), pointer :: i_vc(:)          => null()        ! intercept of photosynthesis vcmax ~ leaf n content regression model
      real(r8), pointer :: s_vc(:)          => null()        ! slope of photosynthesis vcmax ~ leaf n content regression model
-     real(r8), pointer :: nsc_rtime(:)     => null()        ! non-structural carbon residence time 
+     real(r8), pointer :: nsc_rtime(:)     => null()        ! non-structural carbon residence time
      real(r8), pointer :: pinit_beta1(:)   => null()        ! shaping parameter for P initialization
      real(r8), pointer :: pinit_beta2(:)   => null()        ! shaping parameter for P initialization
      real(r8), pointer :: alpha_nfix(:)    => null()        ! fraction of fixed N goes directly to plant
@@ -144,8 +144,11 @@ module VegetationPropertiesType
      real(r8), pointer :: br_xr(:)         => null()   !Base rate for excess respiration
      real(r8), pointer :: tc_stress        => null()   !Critial temperature for moisture stress
 
-     !NGEE Arctic
-     real(r8), pointer :: bend_parm(:)     => null()   ! shrub bending parameter
+     ! NGEE Arctic snow-vegetation interactions
+     real(r8), pointer :: bendresist(:)       ! vegetation resistance to bending under snow loading, 0 to 1 (e.g., Liston and Hiemstra 2011)
+     real(r8), pointer :: vegshape(:)         ! shape parameter to modify shrub burial by snow (1 = parabolic, 2 = hemispheric)
+     real(r8), pointer :: stocking(:)         ! stocking density for pft (stems / hectare)
+     real(r8), pointer :: taper(:)            ! ratio of height:radius_breast_height (woody vegetation allometry)
 
    contains
    procedure, public :: Init => veg_vp_init
@@ -183,6 +186,7 @@ contains
     use pftvarcon , only : fnr, act25, kcha, koha, cpha, vcmaxha, jmaxha, tpuha
     use pftvarcon , only : lmrha, vcmaxhd, jmaxhd, tpuhd, lmrse, qe, theta_cj
     use pftvarcon , only : bbbopt, mbbopt, nstor, br_xr, tc_stress, lmrhd
+    use pftvarcon , only : bendresist, stocking, vegshape, taper ! NGEE Arctic IM3
     !
 
     class (vegetation_properties_type) :: this
@@ -307,6 +311,11 @@ contains
     allocate(this%tc_stress    )
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+    ! NGEE Arctic show-vegetation interactions
+    allocate(this%bendresist(0:numpft))                          ; this%bendresist(:)            =spval
+    allocate(this%vegshape(0:numpft))                            ; this%vegshape(:)              =spval
+    allocate(this%stocking(0:numpft))                            ; this%stocking(:)              =spval
+    allocate(this%taper(0:numpft))                               ; this%taper(:)                 =spvale
 
     do m = 0,numpft
 
@@ -449,6 +458,13 @@ contains
     this%lamda_ptase   = lamda_ptase
     this%tc_stress     = tc_stress
 
+    ! NGEE Arctic - snow/vegetation interactions
+    do m = 0, numpft ! RPF - move up to earlier pft loops?
+      this%bendresist(m)  = bendresist(m)
+      this%vegshape(m)    = vegshape(m)
+      this%stocking(m)    = stocking(m)
+      this%taper(m)       = taper(m)
+    end do
   end subroutine veg_vp_init
 
 end module VegetationPropertiesType

rfiorella/test-E3SM

components/elm/src/data_types/VegetationPropertiesType.F90
@@ -115,7 +115,7 @@ module VegetationPropertiesType
      real(r8), pointer :: lamda_ptase              => null()! critical value that incur biochemical production
      real(r8), pointer :: i_vc(:)          => null()        ! intercept of photosynthesis vcmax ~ leaf n content regression model
      real(r8), pointer :: s_vc(:)          => null()        ! slope of photosynthesis vcmax ~ leaf n content regression model
-     real(r8), pointer :: nsc_rtime(:)     => null()        ! non-structural carbon residence time 
+     real(r8), pointer :: nsc_rtime(:)     => null()        ! non-structural carbon residence time
      real(r8), pointer :: pinit_beta1(:)   => null()        ! shaping parameter for P initialization
      real(r8), pointer :: pinit_beta2(:)   => null()        ! shaping parameter for P initialization
      real(r8), pointer :: alpha_nfix(:)    => null()        ! fraction of fixed N goes directly to plant
@@ -144,8 +144,11 @@ module VegetationPropertiesType
      real(r8), pointer :: br_xr(:)         => null()   !Base rate for excess respiration
      real(r8), pointer :: tc_stress        => null()   !Critial temperature for moisture stress
 
-     !NGEE Arctic
-     real(r8), pointer :: bend_parm(:)     => null()   ! shrub bending parameter
+     ! NGEE Arctic snow-vegetation interactions
+     real(r8), pointer :: bendresist(:)       ! vegetation resistance to bending under snow loading, 0 to 1 (e.g., Liston and Hiemstra 2011)
+     real(r8), pointer :: vegshape(:)         ! shape parameter to modify shrub burial by snow (1 = parabolic, 2 = hemispheric)
+     real(r8), pointer :: stocking(:)         ! stocking density for pft (stems / hectare)
+     real(r8), pointer :: taper(:)            ! ratio of height:radius_breast_height (woody vegetation allometry)
 
    contains
    procedure, public :: Init => veg_vp_init
@@ -183,6 +186,7 @@ contains
     use pftvarcon , only : fnr, act25, kcha, koha, cpha, vcmaxha, jmaxha, tpuha
     use pftvarcon , only : lmrha, vcmaxhd, jmaxhd, tpuhd, lmrse, qe, theta_cj
     use pftvarcon , only : bbbopt, mbbopt, nstor, br_xr, tc_stress, lmrhd
+    use pftvarcon , only : bendresist, stocking, vegshape, taper ! NGEE Arctic IM3
     !
 
     class (vegetation_properties_type) :: this
@@ -307,6 +311,11 @@ contains
     allocate(this%tc_stress    )
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+    ! NGEE Arctic show-vegetation interactions
+    allocate(this%bendresist(0:numpft))                          ; this%bendresist(:)            =spval
+    allocate(this%vegshape(0:numpft))                            ; this%vegshape(:)              =spval
+    allocate(this%stocking(0:numpft))                            ; this%stocking(:)              =spval
+    allocate(this%taper(0:numpft))                               ; this%taper(:)                 =spval
 
     do m = 0,numpft
 
@@ -449,6 +458,13 @@ contains
     this%lamda_ptase   = lamda_ptase
     this%tc_stress     = tc_stress
 
+    ! NGEE Arctic - snow/vegetation interactions
+    do m = 0, numpft ! RPF - move up to earlier pft loops?
+      this%bendresist(m)  = bendresist(m)
+      this%vegshape(m)    = vegshape(m)
+      this%stocking(m)    = stocking(m)
+      this%taper(m)       = taper(m)
+    end do
   end subroutine veg_vp_init
 
 end module VegetationPropertiesType